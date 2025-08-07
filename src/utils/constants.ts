e24e6703b8dadfc01ecd02ac9e35548fcdb49efc
incepthink <incepthink@gmail.com>
2025-08-07 16:59:32 +0530

    fix: changed backend url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "https://aggtrade-backend.onrender.com"; //https://aggtrade-backend.onrender.com || http://localhost:5000
+export const BACKEND_URL = "https://aggtrade-backend-1.onrender.com"; //https://aggtrade-backend.onrender.com || http://localhost:5000
